76ae44da4d65fb34a8d5ee9cc7ed9b4574ef4006
Lucas Souza <lucassouzzamarques@gmail.com>
2022-01-05 16:58:00 -0300

    Exportando dados com output

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -7,13 +7,14 @@ import { Component } from '@angular/core';
 })
 
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
 
   transferir() {
-    console.log("Solicitada nova transferência")
-    console.log("Valor:", this.valor)
-    console.log("Destino:", this.destino)
+    console.log("Solicitada nova transferência");
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
   }
 }
